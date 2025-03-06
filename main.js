2fc551fcf5d045ae4e9c08ad24b281c63297fb3b
Chu Van Nam <chuvannam@vnpt.vn>
2025-03-06 10:50:38 +0700

    feat(the anh): aaaaa

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-Tran the anh
+const name = 'Tran the anh';
+console.log(name);
+
+// In JavaScript, từ khóa 'const' cho phép bạn khai báo biến chỉ đ��c, tức là bạn không thể gán giá trị mới cho biến này.
